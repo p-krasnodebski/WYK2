47feb14786069354787428a7b62ab819c86cfb60
p-krasnodebski <przemekdaniel@interia.eu>
2021-03-28 14:29:38 +0200

    stage - opcje

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
 
@@ -11,6 +12,17 @@ public class App extends Application {
       Parent root = FXMLLoader.load(getClass().getResource("example.fxml"));
       primaryStage.setTitle("Example FXML Window");
       primaryStage.setScene(new Scene(root));
+
+//      primaryStage.setWidth(200);
+//      primaryStage.setHeight(200);
+
+//      primaryStage.setResizable(false);//stały rozmiar okna
+//        primaryStage.setFullScreen(true);
+//        primaryStage.setX(0);
+//        primaryStage.setY(0); //miejsce uruchomienia
+
+//      primaryStage.initStyle(StageStyle.UNDECORATED);
+      primaryStage.setOpacity(0.5); //przeźroczyste
       primaryStage.show();
     }
 
